05c7073351cbfdb1d257503e4f4286999a056b65
surapan kulsavert <505398@pea.co.th>
2019-03-06 10:57:46 +0700

    add alert 7 days for C2 - Chonburi

--- daily_alert_task.php
+++ daily_alert_task.php
@@ -13,8 +13,8 @@
     if(isWeekend($todaydate) || mysqli_num_rows($holiday_list) > 0){
         return;
     }
-
-    $fetch_group_list = "SELECT group_id FROM tbl_line_group WHERE status = 'A'";
+//Alert 10 Days
+    $fetch_group_list = "SELECT group_id FROM tbl_line_group WHERE status = 'A' AND alert_7days = 'N'";
     $group_list = mysqli_query($conn, $fetch_group_list);
 
     $fetch_existing_complaint = "SELECT main_office, COUNT(main_office) AS count_complaint ".
@@ -30,6 +30,43 @@
         $messages = [
             "type"=> "text",
             "text"=> "Daily Alert :\n\nไม่มีข้อร้องเรียนสถานะกำลังดำเนินการหรือรอดำเนินการที่มากกว่าเท่ากับ 10 วัน ในวันที่ ".DateThai(date("Y-m-d"))
+        ];
+    }
+
+    while($group = $group_list->fetch_assoc()){
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $data = [
+            'to' => $group['group_id'],
+            'messages' => [$messages]
+        ];
+        $post = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+//Alert 7 Days
+    $fetch_group_list = "SELECT group_id FROM tbl_line_group WHERE status = 'A' AND alert_7days = 'Y'";
+    $group_list = mysqli_query($conn, $fetch_group_list);
+
+    $fetch_existing_complaint = "SELECT main_office, COUNT(main_office) AS count_complaint ".
+                                "FROM tbl_complaint ".
+                                "WHERE office_code like 'H%' AND number_of_day>='7' AND complaint_status <> 'ปิด' ".
+                                "GROUP BY main_office ".
+                                "HAVING COUNT(main_office) > 0 ".
+                                "ORDER BY main_office ASC";
+    $complaint_list = mysqli_query($conn, $fetch_existing_complaint);
+    if(mysqli_num_rows($complaint_list) > 0){
+        $messages = getBubbleMessages($conn, DateThai(date("Y-m-d")), $complaint_list);
+    } else {
+        $messages = [
+            "type"=> "text",
+            "text"=> "Daily Alert :\n\nไม่มีข้อร้องเรียนสถานะกำลังดำเนินการหรือรอดำเนินการที่มากกว่าเท่ากับ 7 วัน ในวันที่ ".DateThai(date("Y-m-d"))
         ];
     }
 
